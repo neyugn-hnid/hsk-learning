8b1e1124e0eb04e1f01a685139c2b7042c776c83
neyugn-hnid <nvandinh.dev@gmail.com>
2026-06-11 00:25:59 +0700

    fix: No database host

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,9 +1,8 @@
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { PrismaNeonHTTP } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-const sql = neon(process.env.DATABASE_URL!);
-const adapter = new PrismaNeon(sql);
+const adapter = new PrismaNeonHTTP(process.env.DATABASE_URL!);
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
