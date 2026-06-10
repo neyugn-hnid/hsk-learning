1b6fa064d099c0cf4ef8ea9a855ffdd5c1841afd
neyugn-hnid <nvandinh.dev@gmail.com>
2026-06-11 00:47:26 +0700

    fix: Transactions are not supported in HTTP mode

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
-import { PrismaNeonHTTP } from "@prisma/adapter-neon";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaNeonHTTP(process.env.DATABASE_URL!);
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
